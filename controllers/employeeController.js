f3bd1db7492a33cf8ae29ae2f74d570700c31b0d
adham mohamed <adhamdr10@gmail.com>
2026-02-14 18:49:16 +0200

    Create Method delete Employee

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -42,7 +42,16 @@ export const deleteEmployee = async (req, res) => {
         const sqlText = 'DELETE FROM employees WHERE id = $1 RETURNING *';
         const values = [id];
         const result = await query(sqlText, values);
-        res.status(200).json(result.rows[0]);
+
+        if (result.rowCount === 0) {
+            return res.status(404).json({ error: "Employee not found" });
+        }
+
+        res.status(200).json({ 
+            message: "Employee deleted successfully", 
+            deletedEmployee: result.rows[0] 
+        });
+        
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: "Server Error" });        
--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getEmployees, createEmployee } from '../controllers/employeeController.js'; 
+import { getEmployees, createEmployee , deleteEmployee } from '../controllers/employeeController.js'; 
 
 const router = express.Router();
 
 router.get('/', getEmployees);
 router.post('/', createEmployee);
+router.delete('/:id', deleteEmployee);
 
 export default router;
