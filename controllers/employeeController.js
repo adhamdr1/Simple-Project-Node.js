3b86e8fbff8c4b422e9b6123956d7391c60e33dc
adham mohamed <adhamdr10@gmail.com>
2026-02-14 19:02:55 +0200

    Create Method Update Employee

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -3,7 +3,7 @@ import { query } from '../db.js';
 // Get all employees
 export const getEmployees = async (req, res) => {
     try {
-        const result = await query('SELECT * FROM employees');
+        const result = await query('SELECT * FROM employees ORDER BY id');
         res.status(200).json(result.rows);
     } catch (err) {
         console.error(err);
@@ -34,6 +34,33 @@ export const createEmployee = async (req, res) => {
     }
 };
 
+// update employee
+export const updateEmployee = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, email, position, salary, department_id } = req.body;
+        const sqlText = `
+            UPDATE employees
+            SET 
+                name = COALESCE($1, name),           
+                email = COALESCE($2, email),         
+                position = COALESCE($3, position),
+                salary = COALESCE($4, salary),
+                department_id = COALESCE($5, department_id)
+            WHERE id = $6
+            RETURNING *;
+        `;
+        const values = [name, email, position, salary, department_id, id];
+        const result = await query(sqlText, values);
+        if (result.rowCount === 0) {
+            return res.status(404).json({ error: "Employee not found" });
+        }
+        res.status(200).json(result.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Server Error" });
+    }
+};  
 
 // delete employee
 export const deleteEmployee = async (req, res) => {
@@ -51,7 +78,7 @@ export const deleteEmployee = async (req, res) => {
             message: "Employee deleted successfully", 
             deletedEmployee: result.rows[0] 
         });
-        
+
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: "Server Error" });        
